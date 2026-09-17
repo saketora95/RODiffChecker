a7ceba33cef5d1263ad38db7123b13f3e0a73fd9
95 <jakeasa123@gmail.com>
2026-09-17 15:49:30 +0800

    修正 processSpecificFile 會因為缺少資料夾而無法執行的問題

--- processSpecificFile.py
+++ processSpecificFile.py
@@ -2,6 +2,7 @@ import os
 import sys
 import datetime
 
+import function.file_process as fp
 import function.iteminfo_effect_replace as iteminfo_replace
 
 ENCODING = 'utf-8'
@@ -10,5 +11,7 @@ EXECUTE_PATH = os.path.abspath(os.path.dirname(__file__)) + '\\'
 if getattr(sys, 'frozen', False):
     EXECUTE_PATH = os.path.dirname(sys.executable) + '\\'
 OUTPUT_PATH = EXECUTE_PATH + 'output\\'
+
+fp.create_dir(OUTPUT_PATH)
 
 iteminfo_replace.replace_file(f'{EXECUTE_PATH}kr_info.txt', f'{OUTPUT_PATH}TargetResult.txt', ENCODING)
